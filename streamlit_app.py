28ab5fabe0ef5dc95e158bc10bd78c6a71f491e4
Rohini <115545076+Sammetagithub@users.noreply.github.com>
2022-10-17 16:00:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,15 +4,12 @@ import requests
 import snowflake.connector
 from urllib.error import URLError
 
-
-
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
-
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 #import pandas
@@ -44,7 +41,7 @@ except URLError as e:
     streamlit.error()
 
 
-#streamlit.stop()
+streamlit.stop()
 #import snowflake.connector
 
 streamlit.header("The fruit load list contains:")
